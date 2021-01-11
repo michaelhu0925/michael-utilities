98ff7eec7c2d32bdfc61e8031480e3af508d6b40
Michael <michaelhu0925@163.com>
2021-01-11 23:01:20 +0800

    poll update

--- poll.js
+++ poll.js
@@ -12,9 +12,16 @@ module.exports = (client) => {
         }, 750);
     }
 
-        client.on('message', message => {
+        client.on('message', async, message => {
             if (channelIds.includes(message.channel.id)) {
                 addReactions(message)
+            } else if (message.content.toLowerCase() === '>poll') {
+                await message.delete()
+
+                const fetched = await message.channel.messages.fetch({ limit: 1 })
+                if (fetched && fetched.first()) {
+                    addReactions(fetched.first())
+                }
             }
         })
 }
