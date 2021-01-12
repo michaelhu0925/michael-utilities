46b026cc8e2742403c8d60bc4cbf6e15059a6f23
Michael <michaelhu0925@163.com>
2021-01-12 10:30:30 +0800

    welcome message

--- welcome.js
+++ welcome.js
@@ -1,5 +1,11 @@
 module.exports = (client) => {
+    const channelId = '730717878046818365 ' //welcome channel
     client.on('guildMemberAdd', (member) => {
         console.log(member)
+
+        const message = `Hey <@${member.id}>, welcome to the server! Please verify at <#730717878046818366> and enjoy your stay.`
+
+        const channel = member.guild.channels.cache.get(channelId)
+        channel.send(message)
     })
 }
