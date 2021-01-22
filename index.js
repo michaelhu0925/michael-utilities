e981b6cfa67813c827b5e265a39674125668c24f
Michael <michaelhu0925@163.com>
2021-01-22 21:04:10 +0800

    bug

--- index.js
+++ index.js
@@ -273,7 +273,7 @@ client.on('ready', async () => {
         message.channel.send(embed)
     })
 
-    command(client, 'restart', message =>{
+    command(client, 'restart', message, async() =>{
         if (message.author.id !== '696211031579688971') {
             return message.channel.send('You do not have permission to use this command!')
         }
