20f5f5dc3b6a8b1c4837e4ec3410a47e6a3ab00b
Michael <michaelhu0925@163.com>
2021-01-22 21:02:15 +0800

    restart command

--- index.js
+++ index.js
@@ -272,6 +272,14 @@ client.on('ready', async () => {
 
         message.channel.send(embed)
     })
+
+    command(client, 'restart', message =>{
+        if (message.author.id !== '696211031579688971') {
+            return message.channel.send('You do not have permission to use this command!')
+        }
+        await message.channel.send('Restarting Bot...')
+        process.exit();
+    })
 })
 
 client.login(process.env.token)
--- restart.js
+++
@@ -1,7 +0,0 @@
-module.exports = (client, message, args) => {
-    if (message.author.id !== '696211031579688971') {
-        return message.channel.send('You do not have permission to use this command!')
-    }
-    await message.channel.send('Restarting Bot...')
-    process.exit();
-}
